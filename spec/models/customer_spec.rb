f33741cff8a22401eac900754755758e80409be3
Noah van Ekdom <noahvanekdom@gmail.com>
2023-01-11 13:01:34 -0700

    Setup rspec

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  describe "Validations" do
+
+  end
+
+  describe "Relationships" do
+
+  end
+
+  describe "Methods" do
+
+  end
+end
